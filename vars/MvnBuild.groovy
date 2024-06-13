1fee78d2800f43b8a00d870e32fffa871b8086c7
jayanth-999 <jayanthnuggu@gmail.com>
2024-06-13 09:59:03 +0530

    updated maven

--- vars/MvnBuild.groovy
+++ vars/MvnBuild.groovy
@@ -1,10 +1,23 @@
 def call(Map params = [:]) {
   echo "Starting Maven build step"
-  mavenGoals = params.goals ?: 'clean package'
-  
+  echo "Operation: ${params.Operation}" // Access the 'Operation' parameter
+
+  // Define Maven goals based on the 'Operation' parameter
+  def mavenGoals
+  if (params.Operation == 'build') {
+    mavenGoals = 'clean package'
+  } else if (params.Operation == 'test') {
+    mavenGoals = 'clean test'
+  } else if (params.Operation == 'deploy') {
+    mavenGoals = 'clean deploy'
+  } else {
+    echo "Invalid operation: ${params.Operation}"
+    return
+  }
+
   try {
     echo "Running Maven build with goals: ${mavenGoals}"
-    bat "mvn ${mavenGoals ?: 'clean package'} ${params.options ?: ''}"
+    bat "mvn ${mavenGoals} ${params.options ?: ''}"
   } catch (Exception e) {
     echo "Maven build failed: ${e.message}"
     // Add error handling logic here (e.g., send notifications, log to a file)
