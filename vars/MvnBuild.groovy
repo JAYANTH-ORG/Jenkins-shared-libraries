70c66b25efba099dd8423a9e81f0accad281d329
jayanth-999 <jayanthnuggu@gmail.com>
2024-06-13 10:32:00 +0530

    updated

--- vars/MvnBuild.groovy
+++ vars/MvnBuild.groovy
@@ -1,26 +1,25 @@
-def call() {
+def call(Operation) {
   echo "Starting Maven build step"
-  bat "mvn clean package"
-  // echo "Operation: ${params.Operation}" // Access the 'Operation' parameter
+  echo "Operation: ${params.Operation}" // Access the 'Operation' parameter
 
-  // // Define Maven goals based on the 'Operation' parameter
-  // def mavenGoals
-  // if (params.Operation == 'build') {
-  //   mavenGoals = 'clean package'
-  // } else if (params.Operation == 'test') {
-  //   mavenGoals = 'clean test'
-  // } else if (params.Operation == 'deploy') {
-  //   mavenGoals = 'clean deploy'
-  // } else {
-  //   echo "Invalid operation: ${params.Operation}"
-  //   return
-  // }
+  // Define Maven goals based on the 'Operation' parameter
+  def mavenGoals
+  if (params.Operation == 'build') {
+    mavenGoals = 'clean package'
+  } else if (params.Operation == 'test') {
+    mavenGoals = 'clean test'
+  } else if (params.Operation == 'deploy') {
+    mavenGoals = 'clean deploy'
+  } else {
+    echo "Invalid operation: ${params.Operation}"
+    return
+  }
 
-  // try {
-  //   echo "Running Maven build with goals: ${mavenGoals}"
-  //   bat "mvn ${mavenGoals} ${params.options ?: ''}"
-  // } catch (Exception e) {
-  //   echo "Maven build failed: ${e.message}"
-  //   // Add error handling logic here (e.g., send notifications, log to a file)
-  // }
+  try {
+    echo "Running Maven build with goals: ${mavenGoals}"
+    bat "mvn ${mavenGoals} ${params.options ?: ''}"
+  } catch (Exception e) {
+    echo "Maven build failed: ${e.message}"
+    // Add error handling logic here (e.g., send notifications, log to a file)
+  }
 }
