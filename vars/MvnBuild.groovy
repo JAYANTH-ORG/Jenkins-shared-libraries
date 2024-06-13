9885c51d5f1c9aec662995811edf9d3c141864ca
jayanth-999 <jayanthnuggu@gmail.com>
2024-06-13 09:54:38 +0530

    updated mavenBuild

--- vars/MvnBuild.groovy
+++ vars/MvnBuild.groovy
@@ -1,5 +1,12 @@
-def call(){
-  echo "starting maven build step"
-  bat 'mvn clean package -DskipTests'  
+def call(Map params = [:]) {
+  echo "Starting Maven build step"
+  mavenGoals = params.goals ?: 'clean package'
+  
+  try {
+    echo "Running Maven build with goals: ${mavenGoals}"
+    bat "mvn ${mavenGoals ?: 'clean package'} ${params.options ?: ''}"
+  } catch (Exception e) {
+    echo "Maven build failed: ${e.message}"
+    // Add error handling logic here (e.g., send notifications, log to a file)
+  }
 }
-
