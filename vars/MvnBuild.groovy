be747c417afc6ef9339ac387f62dcc0edd280083
jayanth-999 <jayanthnuggu@gmail.com>
2024-06-16 20:28:52 +0530

    updated artifactdetails

--- vars/MvnBuild.groovy
+++ vars/MvnBuild.groovy
@@ -18,6 +18,26 @@ def call(Operation) {
   try {
     echo "Running Maven build with goals: ${mavenGoals}"
     bat "mvn ${mavenGoals} ${params.options ?: ''}"
+
+    if (params.Operation == 'build' || params.Operation == 'deploy') {
+      steps.echo "Deploying artifacts to Artifactory"
+      def server = steps.Artifactory.server 'Artifactory-Server'  // Use the ID configured in Jenkins
+      def buildInfo = steps.Artifactory.newBuildInfo()
+      def rtMaven = steps.Artifactory.newMavenBuild()
+      
+      rtMaven.resolver server: server, releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot'
+      rtMaven.deployer server: server, releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot'
+
+      rtMaven.run pom: 'pom.xml', goals: 'deploy', buildInfo: buildInfo
+      server.publishBuildInfo buildInfo
+    }
+
+    // if (params.Operation == 'build' || params.Operation == 'test') {
+    //     steps.echo "Running SonarQube analysis"
+    //     steps.withSonarQubeEnv('SonarQube') {
+    //         steps.sh "mvn sonar:sonar"
+    //     }
+    // }
   } catch (Exception e) {
     echo "Maven build failed: ${e.message}"
     // Add error handling logic here (e.g., send notifications, log to a file)
