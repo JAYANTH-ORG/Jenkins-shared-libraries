9ca1b5107ec12cff364221add219c0366d50d6ec
Jayanth Nuggu <67133310+jayanth-999@users.noreply.github.com>
2024-06-18 19:42:59 +0530

    Create deployToTomcat.groovy

---
+++ vars/deployToTomcat.groovy
@@ -0,0 +1,21 @@
+// vars/deployToTomcat.groovy
+
+def call() {
+    // def tomcatCredentialsId = params.tomcatCredentialsId ?: 'tomcat'
+    // def tomcatUrl = params.tomcatUrl ?: 'http://localhost:8089/'
+    // def contextPath = params.contextPath ?: '/home'
+    // def warPattern = params.warPattern ?: '**/*.war'
+  // vars/deployToTomcat.groovy
+
+    def tomcatCredentialsId =  'tomcat'
+    def tomcatUrl = 'http://localhost:8089/'
+    def contextPath = '/home'
+    def warPattern = '**/*.war'
+
+    deploy adapters: [tomcat9(credentialsId: tomcatCredentialsId, path: '', url: tomcatUrl)], 
+           contextPath: contextPath, 
+           war: warPattern
+}
+
+
+ 
